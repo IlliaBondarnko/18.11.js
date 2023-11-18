1ab5c55d7782256c24faef7800b9fcb2508faba1
IlliaBondarnko <skyarcher007@gmail.com>
2023-11-18 16:24:42 +0200

    almost done

---
+++ index.js
@@ -0,0 +1,92 @@
+// first tasK_________________________________________________________
+
+var celciumT = 2
+var farinheitT = 70
+
+var intoF = celciumT * 1.8000 + 32.00
+var intoC = (farinheitT - 32.00) / 1.8000 
+
+console.log("first task:")
+console.log(intoF, "°F")
+console.log(intoC, "°C")
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// second tasK________________________________________________________
+
+var daysInMonth = 30
+
+var hoursInMonth = 30 * 24
+var minutesInMonth = hoursInMonth * 60
+
+console.log("second task:")
+console.log("Hours in a month: ", hoursInMonth,"hours;", "Minutes in a month: ", minutesInMonth, "min;")
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// third tasK_________________________________________________________
+
+var maxHealth = 100
+var maxStamina = 100
+
+console.log("third task:")
+console.log(maxHealth - 21)
+console.log(maxStamina - 50)
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// fourth tasK________________________________________________________
+
+var totalll = 789
+var discount = totalll / 100 * 10
+
+console.log("fourth task:")
+console.log("total sum with discount: ", totalll - discount)
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// fifth tasK_________________________________________________________
+
+var number = 12.789
+
+console.log("fifth task:")
+console.log(Math.floor(number))
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// sixth tasK_________________________________________________________
+
+var stringNunberAndLetters = "12.789abcdefg"
+
+console.log("sixth task:")
+console.log(parseFloat(stringNunberAndLetters))
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// seventh tasK_______________________________________________________
+
+var stringNumber = "23"
+
+console.log("seventh task:")
+console.log(parseInt(stringNumber))
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// eighth tasK________________________________________________________
+
+var squareNum = 1800964
+
+console.log("eighth task:")
+console.log(Math.sqrt(squareNum))
+console.log("___________________________________")
+
+// ___________________________________________________________________
+// ninth tasK________________________________________________________
+
+var normNum = 1
+var stringNum = "21"
+
+console.log("ninth task:")
+console.log(parseInt(stringNum))
+console.log(toString(normNum))
+console.log("___________________________________")
